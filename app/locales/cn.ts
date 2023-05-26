5a62612911ca49051d1eca078fa0dd134fea7d99
Samciu <samciu@163.com>
2023-05-26 22:16:53 +0800

    修改文案

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "访问密码不正确或为空，请前往[设置](/#/settings)页输入正确的访问密码，或者填入你自己的 OpenAI API Key。",
+      "前往公众号：**代码明明没有错** 回复 **密码** 免费获取密码，然后点击左下角[设置](/#/settings)按钮输入密码后解锁**免费使用**。\n\n![代码明明没有错](/hello.jpg)\n\n👆立即扫码前往",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
@@ -176,7 +176,7 @@ const cn = {
   },
   Store: {
     DefaultTopic: "新的聊天",
-    BotHello: `前往公众号：**代码明明没有错** 回复 **密码** 免费获取密码，然后点击左下角[设置](/#/settings)按钮输入密码后解锁**免费使用**。\n\n![代码明明没有错](/hello.jpg)\n\n👆立即扫码前往`,
+    BotHello: `建议关注：**代码明明没有错** 防止失联\n\n已解锁免费使用，有什么可以帮你的吗？`,
     Error: "出错了，稍后重试吧",
     Prompt: {
       History: (content: string) => "这是历史聊天总结作为前情提要：" + content,
